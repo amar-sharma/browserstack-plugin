751e92d55e0d2f741b0b7d4b7c4503c5c91775b8
Amar Sharma <amarsharma.hacker@gmail.com>
2015-11-03 02:03:51 +0530

    Button working skeleton

--- content/extensions/extension-random.js
+++ content/extensions/extension-random.js
@@ -1,3 +1,30 @@
-var runWithAutomate = function{
- console.log("LOl");
+var runWithAutomate = function(){
+ this.options=SeleniumIDE.Preferences.load();
+ this.options['executeUsingWebDriver']=true;
+ desired_cap = {'browser': 'Chrome', 'browser_version': '43.0', 'os': 'Windows', 'os_version': '8.1', 'resolution': '1024x768'};
+ this.options['webDriverBrowserString']=desired_cap;
+ SeleniumIDE.Preferences.save(this.options);
+ this.options=SeleniumIDE.Preferences.load();
 }
+
+WebdriverBackedSelenium.prototype.startNewWebdriverSession = function(browserName) {
+  var self = this;
+  return new Deferred(function(deferred) {
+    alert('Lel')
+    LOG.debug('Connecting to Selenium Fucker');
+    HTTP.post('http://localhost:4444/wd/hub/session',
+      JSON.stringify({
+        'desiredCapabilities': browserName}),
+      {'Accept': 'application/json; charset=utf-8'},
+      function(response, success) {
+        if (success && response) {
+          self.webdriverResponse = JSON.parse(response.replace(/\0/g, ''));
+          self.webdriverSID = self.webdriverResponse.sessionId;
+          deferred.resolve(self.webdriverSID);
+        } else {
+          deferred.reject({message: {t: 'RemoteConnectError', m: 'Could not connect to Selenium Server. Have you started the Selenium Server yet?'}});
+        }
+      }
+    );
+  });
+};
